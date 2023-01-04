c6627d05b597f3db7e8faff6254c5d1b0b780726
Mathis R <98423827+mathis-r@users.noreply.github.com>
2023-01-04 19:35:21 +0100

    Add files via upload

---
+++ lua/nvim/init.lua
@@ -0,0 +1,3 @@
+require("nvim.packer")
+require("nvim.set")
+require("nvim.remap")
---
+++ lua/nvim/set.lua
@@ -0,0 +1,10 @@
+vim.g.mapleader = " "
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
